625ab530b7fe2d1ee9abe80fdea30446d4577b15
Miguel Costa <mfocosta@outlook.com>
2023-09-15 00:27:49 +0100

    Updated I2C functions

--- main/main.c
+++ main/main.c
@@ -4,43 +4,57 @@
 #include <unistd.h>
 #include "driver/i2c.h"
 #include "Registers.h"
+#include "esp_log.h"
+
 
 
 /* Defines */
+#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_TIMEOUT_MS       1000
 #define I2C_MASTER_SCL_IO 22
 #define I2C_MASTER_SDA_IO 21
-#define I2C_MASTER_NUM I2C_NUM_0
-#define I2C_MASTER_FREQ_HZ 100000
 
-#define ISL94202_ADDR 0x29 // Endereço I2C do ISL94202
+#define ISL94202_ADDR 0x29 /* Endereço I2C do ISL94202 */
+#define CELL_SELECT_REG 0x49
 
+static const char *TAG = "bms-test";
 
+/**
+ * @brief i2c master initialization
+ */
+static esp_err_t i2c_master_init(void)
+{
+    int i2c_master_port = I2C_MASTER_NUM;
 
-void i2c_master_init() {
-    i2c_config_t conf;
-    conf.mode = I2C_MODE_MASTER;
-    conf.sda_io_num = I2C_MASTER_SDA_IO;
-    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-    conf.scl_io_num = I2C_MASTER_SCL_IO;
-    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
-    i2c_param_config(I2C_MASTER_NUM, &conf);
-    i2c_driver_install(I2C_MASTER_NUM, conf.mode,
-                       0, 0, 0);
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+
+    i2c_param_config(i2c_master_port, &conf);
+
+    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
 }
 
 void app_main() {
     uint8_t reg_value;
+    uint8_t cellSelectReg = 0x49;
+    uint8_t write_buf[2] = {0x49, 0b11000011}; /* 4 cells */
     i2c_master_init();
-    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-    i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, (ISL94202_ADDR << 1) | I2C_MASTER_WRITE, true);
-    i2c_master_write_byte(cmd, 0x01, true); // Endereço do registo a ser lido
-    i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, (ISL94202_ADDR << 1) | I2C_MASTER_READ, true);
-    i2c_master_read_byte(cmd, &reg_value, I2C_MASTER_LAST_NACK);
-    i2c_master_stop(cmd);
-    i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
-    i2c_cmd_link_delete(cmd);
-    printf("Valor do registro: %02x\n", reg_value);
+    i2c_master_write_read_device(I2C_MASTER_NUM, ISL94202_ADDR, &cellSelectReg, 1, &reg_value, 1, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+    printf("Valor do registo inicial: %02x\n", reg_value);
+    i2c_master_write_to_device(I2C_MASTER_NUM, ISL94202_ADDR, write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+    i2c_master_write_read_device(I2C_MASTER_NUM, ISL94202_ADDR, &cellSelectReg, 1, &reg_value, 1, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+    printf("Valor do registo apos escrita: %02x\n", reg_value);
+
+
+    ESP_ERROR_CHECK(i2c_driver_delete(I2C_MASTER_NUM));
+    ESP_LOGI(TAG, "I2C de-initialized successfully");
 }
