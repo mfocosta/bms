c7a7b83c0a3ca94b20d744bc830b770e333ec7bd
Miguel Costa <mfocosta@outlook.com>
2023-10-03 15:27:28 +0100

    added freeRTOS

--- main/main.c
+++ main/main.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
-#include "driver/i2c.h"
 #include "Registers.h"
+#include "driver/i2c.h"
 #include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
 
 
 
@@ -14,8 +17,8 @@
 #define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
 #define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
 #define I2C_MASTER_TIMEOUT_MS       1000
-#define I2C_MASTER_SCL_IO 22
-#define I2C_MASTER_SDA_IO 21
+#define I2C_MASTER_SCL_IO           22
+#define I2C_MASTER_SDA_IO           21
 
 static const char *TAG = "bms-test";
 
@@ -40,25 +43,37 @@ static esp_err_t i2c_master_init(void)
     return (i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0));
 }
 
+/**
+ * @brief write to EEprom
+ */
 static esp_err_t writeToEEprom() {
 
 
-	return (false);
+	return (ESP_OK);
 }
 
+/**
+ * @brief write to Register
+ */
+static esp_err_t writeReg(uint8_t regValue, uint8_t regAddress) {
+    ESP_ERROR_CHECK_WITHOUT_ABORT(i2c_master_write_read_device(I2C_MASTER_NUM, ISL94202_ADDR, &regAddress, sizeof(regAddress), &regValue, sizeof(regValue), I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS));
+    printf("Initial %02x register value: %02x\n", regValue);
+    ESP_ERROR_CHECK_WITHOUT_ABORT(i2c_master_write_to_device(I2C_MASTER_NUM, ISL94202_ADDR, write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS));
+    ESP_ERROR_CHECK_WITHOUT_ABORT(i2c_master_write_read_device(I2C_MASTER_NUM, ISL94202_ADDR, &regAddress, sizeof(regAddress), &regValue, sizeof(regValue), I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS));
+    printf("Register %02x value after write: %02x\n", regValue);
 
+	return (ESP_OK);
+}
+
+/**
+ * @brief main function
+ */
 void app_main() {
     uint8_t reg_value;
     uint8_t cellSelectReg = 0x49;
     uint8_t write_buf[2] = {0x49, 0b11000011}; /* 4 cells */
     i2c_master_init();
-    i2c_master_write_read_device(I2C_MASTER_NUM, ISL94202_ADDR, &cellSelectReg, 1, &reg_value, 1, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
-    printf("Initial register value: %02x\n", reg_value);
-    i2c_master_write_to_device(I2C_MASTER_NUM, ISL94202_ADDR, write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
-    i2c_master_write_read_device(I2C_MASTER_NUM, ISL94202_ADDR, &cellSelectReg, 1, &reg_value, 1, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
-    printf("Register value after write: %02x\n", reg_value);
-
-
+    
     ESP_ERROR_CHECK(i2c_driver_delete(I2C_MASTER_NUM));
     ESP_LOGI(TAG, "I2C de-initialized successfully");
 }
